7d7270d8f9842d437c054bd07a1129043f18b796
Mateus Balduino <balduinomateus0@gmail.com>
2026-02-26 15:34:28 -0300

    feat: adicionado suporte a Dockerfile para containerizar o backend Spring boot

--- cadastro/src/main/resources/db/migration/V1__init.sql
+++ cadastro/src/main/resources/db/migration/V1__init.sql
@@ -2,19 +2,20 @@ CREATE type pet_type AS ENUM ('cat', 'dog');
 CREATE type pet_sex AS ENUM ('male', 'female');
 
 CREATE table address (
-    id INT SERIAL PRIMARY KEY,
+    id INTEGER GENERATED ALWAYS AS IDENTITY,
     street VARCHAR(50),
     number INT,
     complement VARCHAR(30),
-    district VARCHAR(50)
+    district VARCHAR(50),
+    CONSTRAINT pk_address PRIMARY KEY (id)
 );
 
 CREATE table pet (
-    id SERIAL PRIMARY KEY,
+    id INTEGER GENERATED ALWAYS AS IDENTITY ,
     name VARCHAR(20),
     type pet_type NOT NULL,
     sex pet_sex NOT NULL,
-    address_id INT, FOREIGN KEY (address_id) REFERENCES address(id),
+    address_id INTEGER, FOREIGN KEY (address_id) REFERENCES address(id),
     age INT,
     weight DECIMAL(5,2),
     race VARCHAR(20)
